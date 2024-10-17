493b90bf448cbe985ac1d56453f69536b6898bbf
buho <l22212153@tectijuana.edu.mx>
2024-10-17 14:03:11 -0700

    creacion de clase para conexion a base de datos

---
+++ vsoccer/conexion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
+
+namespace vsoccer
+{
+    internal class conexion
+    {
+        //Método para abrir conexión
+        public static MySqlConnection conex()
+        {
+            //declaración de cadena para servidor
+            string servidor = "server = localhost; database=vsoccer; Uid = root; pwd=pokechon";
+
+            //Creación de instancia de la clase MySqlConnection
+            MySqlConnection conexionBD = new MySqlConnection(servidor);
+            try
+            {
+                //devolución de conexión creada
+                return conexionBD;
+            }
+            catch (Exception e)
+            {
+                // si ocurre excepción muestra mensaje
+                MessageBox.Show(e.Message+e.StackTrace);
+                return null;
+            }
+
+            
+        }
+    }
+}
