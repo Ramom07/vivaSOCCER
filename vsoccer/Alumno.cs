fbf938f0cf379de925caef057d68271b713d41a9
buho <l22212153@tectijuana.edu.mx>
2024-10-29 11:47:39 -0700

    conexion aun incompleta

---
+++ vsoccer/Alumno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vsoccer
+{
+     class Alumno
+    {
+        private int id;
+        private string nombre;
+        private string categoria;
+        private DateTime fechaNac;
+        private string tutor;
+        private string telefono;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+        public string Tutor { get => tutor; set => tutor = value; }
+        public string Telefono { get => telefono; set => telefono = value; }
+    }
+}
---
+++ vsoccer/CtrlAlumnos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace vsoccer
+{
+     class CtrlAlumnos
+    {
+        public List<Object> consulta(string dato)
+        {
+            MySqlDataReader reader;
+            List<Object> lista = new List<object>();
+            string sql;
+
+            if (dato == null)
+            {
+                sql = 
+            }
+        }
+    }
+}
