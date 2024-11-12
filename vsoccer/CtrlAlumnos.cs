861c9b0fbf89289c043b73e753de6885fceddd3f
buho <l22212153@tectijuana.edu.mx>
2024-11-12 12:55:01 -0800

    cambios yeah

--- vsoccer/CtrlAlumnos.cs
+++ vsoccer/CtrlAlumnos.cs
@@ -15,10 +15,11 @@ namespace vsoccer
             List<object> lista = new List<object>();
             string sql;
 
-            //if (dato == null)
-            //{
-                
-            sql = @"
+            //consulta sql con filtro segun dato
+            if (string.IsNullOrEmpty(dato))
+            {
+
+                sql = @"
         SELECT 
             a.numcontrol, 
             CONCAT(u.nombre, ' ', u.apellido1, ' ', u.apellido2) AS nombre_completo_alumno, 
@@ -36,37 +37,77 @@ namespace vsoccer
             tutores tu ON at.idtutor = tu.idtutor
         JOIN
             usuarios t ON tu.idusuario = t.id; 
-    ";
-            
-            
-            //}
-
-
-            // aqui debe ir
-            // else {} para cuando se busca en el textbos
+               ";
+            }
+            else
+            {
+                sql = @"
+                SELECT 
+                    a.numcontrol, 
+                    CONCAT(u.nombre, ' ', u.apellido1, ' ', u.apellido2) AS nombre_completo_alumno, 
+                    a.categoria, 
+                    u.fechaNacimiento,
+                    CONCAT(t.nombre, ' ', t.apellido1, ' ', t.apellido2) AS nombre_completo_tutor,
+                    t.tel
+                FROM
+                    alumno_tutor at
+                JOIN
+                    alumnos a ON at.numcontrol = a.numcontrol
+                JOIN
+                    usuarios u ON a.id = u.id
+                JOIN
+                    tutores tu ON at.idtutor = tu.idtutor
+                JOIN
+                    usuarios t ON tu.idusuario = t.id 
+                WHERE
+                    CONCAT(u.nombre, ' ', u.apellido1, ' ', u.apellido2) LIKE @dato;
+                ";
+            }
 
 
             try
             {
-                MySqlConnection conexionBD = base.Conex();
-                conexionBD.Open();
-                MySqlCommand comando = new MySqlCommand(sql, conexionBD);
-                reader = comando.ExecuteReader();
-
-                while (reader.Read())
+                //Abrir conexion
+                using (MySqlConnection conexionBD = AbrirConexion())
                 {
-                    Alumno _alumno = new Alumno
+                    if (conexionBD != null)
                     {
-                        Id = int.Parse(reader["numcontrol"].ToString()), // Asegúrate de que 'numcontrol' es el ID correcto
-                        Nombre = reader["nombre_completo_alumno"].ToString(),
-                        Categoria = reader["categoria"].ToString(),
-                        Tutor = reader["nombre_completo_tutor"].ToString(),
-                        Telefono = reader["tel"].ToString() // Cambia aquí si es necesario
-                    };
+                        using (MySqlCommand comando = new MySqlCommand(sql, conexionBD))
+                        {
+                            //Asogmar parametro de busqueda si se proporciona un dato
+                            if (!string.IsNullOrEmpty(dato))
+                            {
+                                comando.Parameters.AddWithValue("@dato", "%" + dato + "%");
+                            }
 
-                    lista.Add(_alumno);
+
+
+
+                            reader = comando.ExecuteReader();
+
+                            while (reader.Read())
+                            {
+                                Alumno _alumno = new Alumno
+                                {
+                                    Id = int.Parse(reader["numcontrol"].ToString()), // Asegúrate de que 'numcontrol' es el ID correcto
+                                    Nombre = reader["nombre_completo_alumno"].ToString(),
+                                    Categoria = reader["categoria"].ToString(),
+                                    Tutor = reader["nombre_completo_tutor"].ToString(),
+                                    Telefono = reader["tel"].ToString() // Cambia aquí si es necesario
+                                };
+
+                                lista.Add(_alumno);
+                            }
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("No se pudo establecer conexion");
+                    }
+
+
                 }
-
             }
             catch (MySqlException ex)
             {
